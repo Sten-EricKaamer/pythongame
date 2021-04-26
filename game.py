09627839463b32a9fb65ac1713b2be0e125326a9
Sten-EricKaamer <sten-eric.kaamer@khk.ee>
2021-04-26 13:20:30 +0300

    ok

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+
+    pygame.init()
+    game_settings = Settings()
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
+    pygame.display.set_caption("Example Game")
+
+    ship = Ship(screen)
+
+    while True:
+        gf.check_events()
+        gf.update_screen(game_settings, screen, ship)
+
+
+run_game()
---
+++ game_functions.py
@@ -0,0 +1,15 @@
+import sys
+import pygame
+
+
+def check_events():
+    """Check keyboard and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(game_settings, screen, ship):
+    screen.fill(game_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
