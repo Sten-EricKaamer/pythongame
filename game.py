67c0105089fa7d00901456d0df7d0935aee6d3e2
Sten-EricKaamer <sten-eric.kaamer@khk.ee>
2021-05-25 20:26:55 +0300

    ok

--- game.py
+++ game.py
@@ -1,22 +1,28 @@
 import pygame
-
+from pygame.sprite import Group
+from game_stats import GameStats
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
-
 def run_game():
-
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Example Game")
+    stats = GameStats(game_settings, screen)
 
-    ship = Ship(screen)
+    ship = Ship(game_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(game_settings, screen, ship, aliens)
 
     while True:
-        gf.check_events()
-        gf.update_screen(game_settings, screen, ship)
-
-
+        gf.check_events(game_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+        gf.update_bullets(aliens, bullets, game_settings, screen, ship)
+        gf.update_aliens(game_settings, aliens, stats, ship, screen, bullets)
+        gf.update_screen(game_settings, screen, ship, aliens, bullets)
 run_game()
---
+++ game_stats.py
@@ -0,0 +1,7 @@
+class GameStats():
+    def __init__(self, game_settings, screen):
+        self.game_settings = game_settings
+        self.game_active = True
+        self.reset_stats()
+    def reset_stats(self):
+        self.ships_left = self.game_settings.ship_limit
